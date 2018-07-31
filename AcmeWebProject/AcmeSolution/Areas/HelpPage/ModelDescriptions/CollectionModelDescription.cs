cd215706bb9b22e613465dc8e30b2709c57f8b7d
Marco Borges <marcoafborges@gmail.com>
2018-07-31 20:28:06 -0300

    Project creation

---
+++ AcmeWebProject/AcmeSolution/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AcmeSolution.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AcmeWebProject/AcmeSolution/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AcmeSolution.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
